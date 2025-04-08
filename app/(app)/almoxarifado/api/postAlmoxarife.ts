f12a094ae375cef5e359c59fd21dff8d0ea0d5b9
GuiFxFarias <guilhermefarias210303@gmail.com>
2025-04-08 19:43:36 -0300

    fix

--- app/(app)/almoxarifado/api/postAlmoxarife.ts
+++ app/(app)/almoxarifado/api/postAlmoxarife.ts
@@ -1,30 +1,31 @@
-export async function postTransmissor(data) {
-  const response = await fetch("http://localhost:3001/prodTransmissor", {
-    method: "POST",
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function postTransmissor(data: any) {
+  const response = await fetch('http://localhost:3001/prodTransmissor', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
   return response.json();
 }
 
-export async function postPosicionador(data) {
-  const response = await fetch("http://localhost:3001/prodPosicionador", {
-    method: "POST",
+export async function postPosicionador(data: any) {
+  const response = await fetch('http://localhost:3001/prodPosicionador', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
   return response.json();
 }
 
-export async function postPecas(data) {
-  const response = await fetch("http://localhost:3001/novasPecas", {
-    method: "POST",
+export async function postPecas(data: any) {
+  const response = await fetch('http://localhost:3001/novasPecas', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
