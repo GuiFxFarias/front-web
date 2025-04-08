52c23c07420a34f29881b3d5b1514474323e4252
GuiFxFarias <guilhermefarias210303@gmail.com>
2025-04-08 19:46:42 -0300

    bugfix type error

--- app/(app)/almoxarifado/registerForm.tsx
+++ app/(app)/almoxarifado/registerForm.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { Checkbox } from "@/components/ui/checkbox";
-import { useState } from "react";
-import TransmissorForm from "./formTransmissor";
-import PosicionadorForm from "./formPositioner";
+import { Checkbox } from '@/components/ui/checkbox';
+import { useState } from 'react';
+import TransmissorForm from './formTransmissor';
+import PosicionadorForm from './formPositioner';
 
 export default function RegisterProductForm() {
   const [isTransmitter, setIsTransmistter] = useState<boolean>();
@@ -12,22 +12,25 @@ export default function RegisterProductForm() {
   return (
     <>
       <h1>O cadastro será de um transmissor ou posicionador?</h1>
-      <div className="flex items-center space-x-2 mt-4 border-b-[1px] pb-2 w-full">
+      <div className='flex items-center space-x-2 mt-4 border-b-[1px] pb-2 w-full'>
         <Checkbox
-          id="transmistter"
-          onCheckedChange={(e) => setIsTransmistter(e)}
+          id='transmistter'
+          onCheckedChange={(e: boolean) => setIsTransmistter(e)}
         />
         <label
-          htmlFor="transmistter"
-          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor='transmistter'
+          className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
         >
           Transmissor
         </label>
 
-        <Checkbox id="positioner" onCheckedChange={(e) => setIsPositioner(e)} />
+        <Checkbox
+          id='positioner'
+          onCheckedChange={(e: boolean) => setIsPositioner(e)}
+        />
         <label
-          htmlFor="positioner"
-          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor='positioner'
+          className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
         >
           Posicionador
         </label>
