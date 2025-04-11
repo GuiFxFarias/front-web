73d13ad60a0137f861ba46f443321554d63bab9a
GuiFxFarias <guilhermefarias210303@gmail.com>
2025-04-11 13:31:52 -0300

    fix mdd

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
+  // const token = req.cookies.get('token')?.value;
 
   const publicRoutes = ['/', '/login', '/usuarios/login'];
   if (publicRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.next();
   }
 
-  if (!token) {
-    console.log('Sem token, redirecionando...');
-    return NextResponse.redirect(new URL('/', req.url));
-  }
+  // if (!token) {
+  //   console.log('Sem token, redirecionando...');
+  //   return NextResponse.redirect(new URL('/', req.url));
+  // }
 
   return NextResponse.next();
 }
