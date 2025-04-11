66b5fe0069f5564f2a15e204d67f5c9686bee90b
GuiFxFarias <guilhermefarias210303@gmail.com>
2025-04-11 10:34:52 -0300

    fix cookie

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
 
+  console.log(token);
+
   if (!token) {
     console.log('Sem token, redirecionando...');
     return NextResponse.redirect(new URL('/', req.url));
