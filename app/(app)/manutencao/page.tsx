d85a202137147572c09be022849ec0be8ed26187
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2025-05-27 21:57:17 -0300

    delete pasta de manutencao

--- app/(app)/manutencao/page.tsx
+++
@@ -1,9 +0,0 @@
-import ManutencaoItens from './manutencaoItens';
-
-export default function ServicosPage() {
-  return (
-    <div className='flex min-h-screen bg-gray-100'>
-      <ManutencaoItens />
-    </div>
-  );
-}
